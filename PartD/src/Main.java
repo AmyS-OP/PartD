a988da8e7538a98d6e2af3b2574e1c29576fafe4
AmyS-OP <AmySutich@myop.ac.nz>
2019-09-16 15:59:46 +1200

    Second line of code

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, my name is Amy");
+		System.out.println("My favourite colour is purple");
 	}
 
 }
